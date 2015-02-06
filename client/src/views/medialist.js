e7203f6ed7074b1243e92dba3adc549b2c6e7130
lakmeer <lakmeerkravid@gmail.com>
2015-02-06 17:29:09 +1300

    Add better buttons, hopefully some state handling

--- client/src/views/medialist.js
+++ client/src/views/medialist.js
@@ -2,8 +2,8 @@ _kiwi.view.MediaList = Backbone.View.extend({
     tagName: "div",
     events: {
         "click .vidni-join": "joinVideoRoom",
-        "click .vidni-stop": "closeVideoRoom",
-        "click .vidni-spectate": "spectateRoom"
+        //"click .vidni-spectate": "spectateRoom",
+        "click .vidni-stop": "closeVideoRoom"
     },
 
     closeVideoRoom: function (event) {
@@ -24,17 +24,20 @@ _kiwi.view.MediaList = Backbone.View.extend({
     initialize: function (options) {
         this.model.bind('all', this.render, this);
         this.model.userState = 'disconnected';
+        this.model.numClients = 0;
         this.$el.appendTo('#kiwi .medialist');
 
+        THIS = this;
+
         console.log('views.medialist::init');
+
         // Holds meta data. User counts, etc
         this.$meta = $('<div class="meta"></div>').appendTo(this.$el);
-        this.renderMetaDisconnected();
+        this.renderMetaDisconnected(this.model);
 
         $remoteVideos = $('<div class="videos">').appendTo(this.$el);
 
         // Icecomm test code
-
         console.log('Polyfill navigator.getUserMedia');
 
         if (navigator.webkitGetUserMedia) {
@@ -46,23 +49,27 @@ _kiwi.view.MediaList = Backbone.View.extend({
         this.ice = new Icecomm(a, { debug: true });
 
         this.ice.on('local', function (opts) {
-          console.log('on local:', opts)
+          console.log('on local:', THIS, opts)
           $remoteVideos.append(opts.video);
           opts.video.className = "local";
+          THIS.renderMetaTransmitting(THIS.model);
         });
 
         this.ice.on('connected', function (opts) {
-          console.log('on connected:', opts)
+          console.log('on connected:', THIS, opts)
           $remoteVideos.append(opts.video);
+          THIS.model.numClients = THIS.ice.getRoomSize();
+          THIS.renderMetaTransmitting(THIS.model);
         });
 
         this.ice.on('disconnect', function (opts) {
-          console.log('on disconnected:', opts)
+          console.log('on disconnected:', THIS, opts)
           $remoteVideos.find('#' + opts.callerID).remove();
+          THIS.model.numClients -= 1;
+          THIS.renderMetaDisconnected(THIS.model);
         });
 
         // END icecomm test code
-
 
         // The list for holding the videos
         this.$list = $('<ul></ul>').appendTo(this.$el);
@@ -80,55 +87,55 @@ _kiwi.view.MediaList = Backbone.View.extend({
 
         switch (this.model.userState) {
           case "disconnected":
-            renderMetaDisconnected();
+            this.renderMetaDisconnected(this.model);
             break;
           case "transmitting":
-            renderMetaTransmitting();
+            this.renderMetaTransmitting(this.model);
             break;
           case "spectating":
-            renderMetaSpectating();
+            this.renderMetaSpectating(this.model);
             break;
           default:
             // nothing
         }
 
         // User count
-        if(this.model.channel.isActive()) {
+        if (this.model.channel.isActive()) {
             this.renderMeta();
         }
 
         return this;
     },
 
-    renderMetaDisconnected: function () {
+    renderMetaDisconnected: function (model) {
       this.$meta.html(
         '<p class="title"> Video Chat</p>' +
         '<div class="status no-rx-no-tx"> Disconnected </div>' +
-        '<div class="status currently"> ?? video users, ?? audio users </div>' +
+        '<div class="status currently">' + model.numClients + ' video users </div>' +
         '<div class="video-tools">' +
-          '<button class="vidni-spectate"><i class="fa fa-eye media_spectate" title="Spectate"></i> Spectate</button>' +
+          '<button class="vidni-spectate disabled"><i class="fa fa-eye media_spectate" title="Spectate"></i> Spectate</button>' +
           '<button class="vidni-join"><i class="fa fa-video-camera media_join" title="Join"></i> Join</button>' +
         '</div>');
     },
 
-    renderMetaSpectating: function () {
+    renderMetaSpectating: function (model) {
       this.$meta.html(
         '<p class="title"> Video Chat</p>' +
         '<div class="status rx-no-tx"> Spectating </div>' +
-        '<div class="status currently"> ?? video users, ?? audio users </div>' +
+        '<div class="status currently">' + model.numClients + ' video users </div>' +
         '<div class="video-tools">' +
           '<button class="vidni-join"><i class="fa fa-video-camera media_join" title="Join"></i> Join</button>' +
           '<button class="vidni-stop"><i class="fa fa-close media_stop" title="Stop"></i> Stop</button>' +
         '</div>');
     },
 
-    renderMetaTransmitting: function () {
+    renderMetaTransmitting: function (model) {
       this.$meta.html(
         '<p class="title"> Video Chat</p>' +
         '<div class="status rx-tx"> Transmitting </div>' +
-        '<div class="status currently"> ?? video users, ?? audio users </div>' +
+        '<div class="status currently">' + model.numClients + ' video users </div>' +
         '<div class="video-tools">' +
-          '<button class="vidni-spectate"><i class="fa fa-eye media_spectate" title="Spectate"></i> Spectate</button>' +
+          '<button class="vidni-spectate disabled"><i class="fa fa-eye media_spectate" title="Spectate"></i> Spectate</button>' +
           '<button class="vidni-stop"><i class="fa fa-close media_stop" title="Stop"></i> Stop</button>' +
         '</div>');
     },
